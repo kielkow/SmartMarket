f1d68082560833097998f41ae2e20436b89340f0
kielkow <matheuskiel@fiorifer.com.br>
2020-04-01 16:53:37 -0300

    Create index.js on database

---
+++ src/database/index.js
@@ -0,0 +1,24 @@
+import Sequelize from 'sequelize';
+
+import Product from '../app/models/Product';
+import Type from '../app/models/Type';
+
+import databaseConfig from '../config/database';
+
+const models = [Product, Type];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+}
+
+export default new Database();
